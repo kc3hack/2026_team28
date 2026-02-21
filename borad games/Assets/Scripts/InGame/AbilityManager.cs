71724e241d6282cc59bce2e07f93133145e28aac
atg192 <2412110402r@kindai.ac.jp>
2026-02-22 05:54:19 +0900

    ボタンを押した際の処理の修正

--- borad games/Assets/Scripts/InGame/AbilityManager.cs
+++ borad games/Assets/Scripts/InGame/AbilityManager.cs
@@ -2,17 +2,6 @@ using UnityEngine;
 
 public class AbilityManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     // ボタンが押された時に実行されるメソッド
     public virtual void OnButtonClick()
